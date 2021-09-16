62f35d0db16b0735e09a73d339fbb6114c0c9540
anaboccanfuso <77176581+anaboccanfuso@users.noreply.github.com>
2021-09-16 13:27:13 -0400

    Added JavaDoc

--- factory/HousePlanFactory.java
+++ factory/HousePlanFactory.java
@@ -1,6 +1,15 @@
 package factory;
 
+/**
+ * A class to create specific house plans based on the user's input. 
+ * @author Ana Boccanfuso
+ */
 public class HousePlanFactory {
+    /**
+     * Creates a house plan based on the string the user enters.
+     * @param type The string the user enters representing the type of house plan they want.
+     * @return Returns the house plan that is created based on the user input.
+     */
     public static HousePlan createHousePlan(String type) {
         HousePlan housePlan = null;
 
--- factory/TinyHomePlan.java
+++ factory/TinyHomePlan.java
@@ -1,11 +1,22 @@
 package factory;
 
+/**
+ * A class that implements the details of a tiny house plan.
+ * @author Ana Boccanfuso
+ */
 public class TinyHomePlan extends HousePlan {
    
+    /**
+     * A method that calls the super with 1 room, 5 windows, and 200 square feet.
+     */
     public TinyHomePlan() {
         super(1, 5, 200);
     }
 
+    /**
+     * A method that adds the materials of the tiny house to the materials
+     * array list.
+     */
     protected void setMaterials() {
         this.materials.add("Lumber");
         this.materials.add("Insulation");
@@ -13,6 +24,10 @@ public class TinyHomePlan extends HousePlan {
         this.materials.add("Hardware");
     }
 
+    /**
+     * A method that adds the features of the tiny house to the materials
+     * array list.
+     */
     protected void setFeatures() {
         this.features.add("Natural Light");
         this.features.add("Creative Storage");
@@ -20,6 +35,10 @@ public class TinyHomePlan extends HousePlan {
         this.features.add("Mutli-Use Applications");
     }
 
+    /**
+     * A method that prints the tiny house information to the user.
+     * @return The string representation of the tiny house information.
+     */
     public String toString() {
         return "Tiny House\n" + super.toString();    
     }
